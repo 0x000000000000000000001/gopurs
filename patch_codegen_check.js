b9beb6b7aaecc6c86e13fcebef8dad4010132bb5
0x1 <kevin.francart3@gmail.com>
2026-07-23 00:06:00 +0200

    misc

--- patch_codegen_check.js
+++
@@ -1,4 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('src/Gopurs/CodeGen.purs', 'utf8');
-let matches = content.match(/translateExprImpl helpersRef (.*?) (false|true|isTail) (nextId[a-zA-Z']*|res[a-zA-Z]*\.nextId|\(nextId \+ 1\)|currNextId|0|acc[a-zA-Z]*\.nextId) /g);
-console.log("Matched: " + (matches ? matches.length : 0));
