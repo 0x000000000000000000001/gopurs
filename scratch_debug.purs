976daacc4ae45a0ee4358a5178e2f0568fb1c14c
0x1 <kevin.francart3@gmail.com>
2026-07-22 20:28:21 +0200

    misc

--- scratch_debug.purs
+++
@@ -1,8 +0,0 @@
-module ScratchDebug where
-
-import Prelude
-import Effect.Console (log)
-
-main = do
-  log $ show 4.0
-  log $ show { a: 1, b: true, c: 'd', e: 4.0 }
--- scratch_ffi.go
+++
@@ -1,7 +0,0 @@
-package ScratchFFI
-import "gopurs/output/gopurs_runtime"
-var ShowImpl = gopurs_runtime.Func(func(showFn gopurs_runtime.Value) gopurs_runtime.Value {
-	return gopurs_runtime.Func(func(val gopurs_runtime.Value) gopurs_runtime.Value {
-		return gopurs_runtime.Apply(showFn, val)
-	})
-})
