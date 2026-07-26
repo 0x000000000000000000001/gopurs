93c398ae98e192a16e5039f8559f49e746241d67
0x1 <kevin.francart3@gmail.com>
2026-07-27 00:31:33 +0200

    misc

--- test_ffi.js
+++
@@ -1,4 +0,0 @@
-import { findFfiFileImpl as f2 } from '../purescript-backend-optimizer/src/PureScript/Backend/Optimizer/FfiSupport.js';
-
-let res = f2(".go")([])(null)("Control.Extend")(".spago/p/control-6.0.0/src/Control/Extend.purs")();
-console.log("Result:", res);
