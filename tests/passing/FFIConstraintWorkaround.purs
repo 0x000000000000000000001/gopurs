a93935ccb473889bec10406e0c6b73db8163f5a4
0x1 <kevin.francart3@gmail.com>
2026-08-09 14:08:26 +0200

    misc

--- tests/passing/FFIConstraintWorkaround.purs
+++ tests/passing/FFIConstraintWorkaround.purs
@@ -7,16 +7,14 @@ import Test.Assert
 
 main :: Effect Unit
 main = do
+  log $ showFFI { a: 1, b: true, c: 'd', e: 4.0 }
   assert' "Showing Int is correct" $ showFFI 4 == "4"
   assert' "Showing String is correct" $ showFFI "string" == "\"string\""
   assert' "Showing Record is correct" $
-    showFFI { a: 1, b: true, c: 'd', e: 4.0 } == "{ a: 1, b: true, c: 'd', e: 4.000000 }"
+    showFFI { a: 1, b: true, c: 'd', e: 4.0 } == "{ a: 1, b: true, c: 'd', e: 4.0 }"
   log "Done"
 
 showFFI :: forall a. Show a => a -> String
 showFFI = showImpl show
 
--- Since type class constraints are not allowed
--- in FFI declarations, we have to pass members
--- we want to use into the function itself.
 foreign import showImpl :: forall a. (a -> String) -> a -> String
