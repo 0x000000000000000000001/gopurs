c8b33cde21e2dd620b36d114930fd50db25fef00
0x1 <kevin.francart3@gmail.com>
2026-08-14 11:25:41 +0200

    misc

--- src/Gopurs/QuoteTAST.purs
+++ src/Gopurs/QuoteTAST.purs
@@ -7,9 +7,9 @@ import Data.Array.NonEmpty as NonEmptyArray
 import Data.Tuple (Tuple(..))
 import Partial.Unsafe (unsafeCrashWith)
 
-import PureScript.Backend.Optimizer.CoreFn (Ann(..), Bind(..), Binding(..), CaseAlternative(..), CaseGuard(..), Expr(..), ExprType(..), Guard(..), Ident(..), Prop(..), Qualified(..))
+import PureScript.Backend.Optimizer.CoreFn (Ann(..), Bind(..), Binding(..), CaseAlternative(..), CaseGuard(..), Expr(..), ExprType(..), Guard(..), Ident(..), Prop(..), Qualified(..), Binder(..), Literal(..))
 import PureScript.Backend.Optimizer.Semantics (BackendExpr(..))
-import PureScript.Backend.Optimizer.Syntax (BackendSyntax(..), Level(..), BackendAccessor(..))
+import PureScript.Backend.Optimizer.Syntax (BackendSyntax(..), Level(..), BackendAccessor(..), fstPair, sndPair)
 
 quoteTAST :: BackendExpr -> Expr Ann
 quoteTAST = case _ of
@@ -40,17 +40,60 @@ quoteTAST = case _ of
           EffectBind ident _ val body -> ExprLet ann [ NonRec (Binding ann (fromMaybe (Ident "_") ident) (quoteTAST val)) ] (quoteTAST body)
           EffectPure e -> quoteTAST e
           EffectDefer e -> quoteTAST e
-          Branch _ _ -> 
-            -- Note: Branch translation is non-trivial if it comes from Case. 
-            -- But we don't have pattern matching natively in Branch, it's just booleans or simple conditions.
-            -- For now we just crash, since we need to see how to reconstruct ExprCase.
-            unsafeCrashWith "quoteTAST: Branch translation is not yet fully implemented"
+          Branch pairs def -> 
+            Array.foldr (\pair acc ->
+              let cond = quoteTAST (fstPair pair)
+                  res = quoteTAST (sndPair pair)
+              in ExprCase ann [cond]
+                   [ CaseAlternative [BinderLit ann (LitBoolean true)] (Unconditional res)
+                   , CaseAlternative [BinderLit ann (LitBoolean false)] (Unconditional acc)
+                   ]
+            ) (quoteTAST def) (NonEmptyArray.toArray pairs)
           PrimOp _ -> unsafeCrashWith "quoteTAST: PrimOp not yet supported"
           PrimEffect _ -> unsafeCrashWith "quoteTAST: PrimEffect not yet supported"
           PrimUndefined -> unsafeCrashWith "quoteTAST: PrimUndefined not yet supported"
           Fail err -> unsafeCrashWith ("quoteTAST: Fail - " <> err)
-          Typed _ _ -> unsafeCrashWith "quoteTAST: nested Typed"
+          Typed _ e -> quoteTAST e
         ExprRewrite _ _ -> unsafeCrashWith "quoteTAST: expected syntax inside Typed, found rewrite"
-    _ -> unsafeCrashWith "quoteTAST: Expected Typed syntax"
+    -- If it's not a Typed at the top level, we just recurse if possible or wrap it
+    -- but usually Semantics wraps everything in Typed. If not, we lack the type.
+    _ -> 
+      let dummyAnn = Ann { span: { path: "", start: { line: 0, column: 0 }, end: { line: 0, column: 0 } }, meta: Nothing, type: Nothing }
+      in case syntax of
+        Var qual -> ExprVar dummyAnn qual
+        Local ident _ -> ExprVar dummyAnn (Qualified Nothing (fromMaybe (Ident "_") ident))
+        Lit lit -> ExprLit dummyAnn (map quoteTAST lit)
+        App f args -> Array.foldl (ExprApp dummyAnn) (quoteTAST f) (map quoteTAST (NonEmptyArray.toArray args))
+        UncurriedApp f args -> Array.foldl (ExprApp dummyAnn) (quoteTAST f) (map quoteTAST args)
+        Abs args body -> Array.foldr (\(Tuple ident _) b -> ExprAbs dummyAnn (fromMaybe (Ident "_") ident) b) (quoteTAST body) (NonEmptyArray.toArray args)
+        UncurriedAbs args body -> Array.foldr (\(Tuple ident _) b -> ExprAbs dummyAnn (fromMaybe (Ident "_") ident) b) (quoteTAST body) args
+        UncurriedEffectApp f args -> Array.foldl (ExprApp dummyAnn) (quoteTAST f) (map quoteTAST args)
+        UncurriedEffectAbs args body -> Array.foldr (\(Tuple ident _) b -> ExprAbs dummyAnn (fromMaybe (Ident "_") ident) b) (quoteTAST body) args
+        Let ident _ val body -> ExprLet dummyAnn [ NonRec (Binding dummyAnn (fromMaybe (Ident "_") ident) (quoteTAST val)) ] (quoteTAST body)
+        LetRec _ binds body -> ExprLet dummyAnn [ Rec (map (\(Tuple ident val) -> Binding dummyAnn ident (quoteTAST val)) (NonEmptyArray.toArray binds)) ] (quoteTAST body)
+        Accessor e (GetProp prop) -> ExprAccessor dummyAnn (quoteTAST e) prop
+        Accessor e (GetIndex idx) -> ExprAccessor dummyAnn (quoteTAST e) (show idx)
+        Accessor e (GetCtorField _ _ _ _ prop _) -> ExprAccessor dummyAnn (quoteTAST e) prop
+        Update e props -> ExprUpdate dummyAnn (quoteTAST e) (map (\(Prop p v) -> Prop p (quoteTAST v)) props)
+        CtorSaturated _ _ pn ident fields -> 
+          Array.foldl (ExprApp dummyAnn) (ExprConstructor dummyAnn pn ident (map (\(Tuple k _) -> k) fields)) (map (\(Tuple _ v) -> quoteTAST v) fields)
+        CtorDef _ pn ident fields -> ExprConstructor dummyAnn pn ident fields
+        EffectBind ident _ val body -> ExprLet dummyAnn [ NonRec (Binding dummyAnn (fromMaybe (Ident "_") ident) (quoteTAST val)) ] (quoteTAST body)
+        EffectPure e -> quoteTAST e
+        EffectDefer e -> quoteTAST e
+        Branch pairs def -> 
+          Array.foldr (\pair acc ->
+            let cond = quoteTAST (fstPair pair)
+                res = quoteTAST (sndPair pair)
+            in ExprCase dummyAnn [cond]
+                 [ CaseAlternative [BinderLit dummyAnn (LitBoolean true)] (Unconditional res)
+                 , CaseAlternative [BinderLit dummyAnn (LitBoolean false)] (Unconditional acc)
+                 ]
+          ) (quoteTAST def) (NonEmptyArray.toArray pairs)
+        PrimOp _ -> unsafeCrashWith "quoteTAST: PrimOp not yet supported"
+        PrimEffect _ -> unsafeCrashWith "quoteTAST: PrimEffect not yet supported"
+        PrimUndefined -> unsafeCrashWith "quoteTAST: PrimUndefined not yet supported"
+        Fail err -> unsafeCrashWith ("quoteTAST: Fail - " <> err)
+        Typed _ e -> quoteTAST e
   ExprRewrite _ _ ->
     unsafeCrashWith "quoteTAST: ExprRewrite encountered"
