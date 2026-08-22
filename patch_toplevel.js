3224d49d170b961d0e25d52709a8b7c446eca808
0x1 <kevin.francart3@gmail.com>
2026-08-22 12:36:17 +0200

    misc

--- patch_toplevel.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('bin/gopurs.js', 'utf8');
-code = code.replace('const v1 = optimize((() => {', `
-  console.log("Optimizing:", v._2);
-  const v1 = optimize((() => {
-`);
-fs.writeFileSync('bin/gopurs.js', code);
