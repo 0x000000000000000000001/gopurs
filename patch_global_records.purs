5baab3668358b89ea8a3fb8b4aaf13efe466602c
0x1 <kevin.francart3@gmail.com>
2026-08-24 00:05:45 +0200

    misc

--- patch_global_records.purs
+++
@@ -1,5 +0,0 @@
-globalRecordStructs :: Ref.Ref (Set.Set String)
-globalRecordStructs = unsafePerformEffect (Ref.new Set.empty)
-
-globalRecordDecls :: Ref.Ref (Array String)
-globalRecordDecls = unsafePerformEffect (Ref.new [])
--- patch_record.go
+++
@@ -1,5 +0,0 @@
-package main
-import "fmt"
-func main() {
-    fmt.Println("This is a scratch file")
-}
