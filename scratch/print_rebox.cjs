9977691079252843525cbb8fcfa44f3bf4fc9bdd
0x1 <kevin.francart3@gmail.com>
2026-09-02 11:45:44 +0200

    misc

--- scratch/print_rebox.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('../../altbak.pub/output/purescript/Control_Comonad_Env_Class.go', 'utf-8');
-let match = code.match(/func Rebox_Control_Comonad_Env_Class_.*?\{[\s\S]*?\}/);
-if (match) {
-  console.log(match[0]);
-} else {
-  console.log("Rebox not found");
-}
--- scratch/verify_box.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const path = 'src/Gopurs/CodeGen.purs';
-let content = fs.readFileSync(path, 'utf8');
-const lines = content.split('\n');
-
-const coerceStart = lines.findIndex(l => l.startsWith('coerceGoExpr '));
-console.log(lines.slice(coerceStart, coerceStart + 25).join('\n'));
