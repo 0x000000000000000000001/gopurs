75b2a3ae75e0ce47cf94b870f44a8fcbc06a0a3b
0x1 <kevin.francart3@gmail.com>
2026-09-13 22:47:26 +0200

    misc

--- tests/passing-snapshots/test-int.go
+++ tests/passing-snapshots/test-int.go
@@ -11,20 +11,20 @@ var once_Main_main sync.Once
 func Get_Main_main() gopurs_runtime.Value {
 	once_Main_main.Do(func() {
 		cache_Main_main = func() gopurs_runtime.Value {
-			// TAST (Let): __local_var_0_0 shape=App(Var) bindingType=(ADT ["Data","Maybe","Maybe"] [Int])
-			__local_var_0_0 := gopurs_runtime.CoerceToStruct[Constructor_Data_Maybe_Just[int64]](gopurs_runtime.Apply(Get_Data_Int_fromNumber(), gopurs_runtime.Float(gopurs_runtime.Float(-2147483648.0).FloatVal())))
-			_ = __local_var_0_0
+			// TAST (Let): v_0_0 shape=App(Var) bindingType=(ADT ["Data","Maybe","Maybe"] [Int])
+			v_0_0 := Rebox_Main_3094389156_1170268447(gopurs_runtime.CoerceToStruct[Constructor_Data_Maybe_Just[gopurs_runtime.Value]](gopurs_runtime.Apply(Get_Data_Int_fromNumber(), gopurs_runtime.Float(-2147483648.0))))
+			_ = v_0_0
 			var __t1 gopurs_runtime.Value
 			{
-				if __local_var_0_0 != nil {
-					__t1 = gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str(("Just ")+(gopurs_runtime.Apply(Get_Data_Show_showIntImpl(), gopurs_runtime.Int((__local_var_0_0).V0.IntVal)).StrVal())))
+				if v_0_0 != nil {
+					__t1 = gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str(("Just ")+(gopurs_runtime.Apply(Get_Data_Show_showIntImpl(), gopurs_runtime.Int((v_0_0).V0)).StrVal())))
 					goto end_branch_1
 				} else {
 
 				}
 			}
 			{
-				if __local_var_0_0 == nil {
+				if v_0_0 == nil {
 					__t1 = gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str("Nothing"))
 					goto end_branch_1
 				} else {
@@ -39,4 +39,13 @@ func Get_Main_main() gopurs_runtime.Value {
 		}()
 	})
 	return cache_Main_main
+}
+
+func Rebox_Main_3094389156_1170268447(in *Constructor_Data_Maybe_Just[gopurs_runtime.Value]) *Constructor_Data_Maybe_Just[int64] {
+	if in == nil {
+		return nil
+	}
+	out := &Constructor_Data_Maybe_Just[int64]{}
+	out.V0 = in.V0.IntVal
+	return out
 }
--- tests/passing-snapshots/test_contains.go
+++ tests/passing-snapshots/test_contains.go
@@ -10,16 +10,11 @@ var once_Main_main sync.Once
 
 func Get_Main_main() gopurs_runtime.Value {
 	once_Main_main.Do(func() {
-		cache_Main_main = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value {
-			// TAST (Let): __local_var_0_0 shape=App(Var) bindingType=Any
-			__local_var_0_0 := gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str("Testing contains"))
-			_ = __local_var_0_0
-			_dollar___unused_1_1 := gopurs_runtime.Apply(__local_var_0_0, gopurs_runtime.Value{})
-			_ = _dollar___unused_1_1
-			_dollar___unused_2_2 := gopurs_runtime.Apply(gopurs_runtime.Apply(Get_Test_Assert_assert(), gopurs_runtime.Bool((gopurs_runtime.Apply2(Get_Data_String_CodeUnits_contains(), gopurs_runtime.Str("interface conversion"), gopurs_runtime.Str("interface conversion: interface {} is nil, not map[string]gopurs_runtime.Value")).IntVal) != (0))), gopurs_runtime.Value{})
-			_ = _dollar___unused_2_2
-			return gopurs_runtime.Apply(gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str("Success")), gopurs_runtime.Value{})
-		})
+		cache_Main_main = gopurs_runtime.Apply2(Call_Control_Bind_bind(gopurs_runtime.CoerceToStruct[Constructor_Control_Bind_Bind[gopurs_runtime.Value]](Get_Effect_bindEffect())), gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str("Testing contains")), gopurs_runtime.Func(func(_dollar___unused_0 gopurs_runtime.Value) gopurs_runtime.Value {
+			return gopurs_runtime.Apply2(Call_Control_Bind_bind(gopurs_runtime.CoerceToStruct[Constructor_Control_Bind_Bind[gopurs_runtime.Value]](Get_Effect_bindEffect())), gopurs_runtime.Apply(Get_Test_Assert_assert(), gopurs_runtime.Bool((gopurs_runtime.Apply3(Call_Control_Semigroupoid_compose(gopurs_runtime.CoerceToStruct[Constructor_Control_Semigroupoid_Semigroupoid[gopurs_runtime.Value]](Get_Control_Semigroupoid_semigroupoidFn())), Get_Data_Maybe_isJust(), gopurs_runtime.Apply(Get_Data_String_CodeUnits_indexOf(), gopurs_runtime.Str("interface conversion")), gopurs_runtime.Str("interface conversion: interface {} is nil, not map[string]gopurs_runtime.Value")).IntVal) != (0))), gopurs_runtime.Func(func(_dollar___unused_1 gopurs_runtime.Value) gopurs_runtime.Value {
+				return gopurs_runtime.Apply(Get_Effect_Console_log(), gopurs_runtime.Str("Success"))
+			}))
+		}))
 	})
 	return cache_Main_main
 }
